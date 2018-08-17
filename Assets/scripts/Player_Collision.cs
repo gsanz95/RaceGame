861d6e04c242dbe915ceb66ecf4eae4147145d15
Giancarlo Sanz <gsanz95@gmail.com>
2018-08-16 22:40:44 -0500

    Added Jumping
    Cube can no longer move in the air

--- Assets/scripts/Player_Collision.cs
+++ Assets/scripts/Player_Collision.cs
@@ -4,10 +4,19 @@ public class Player_Collision : MonoBehaviour {
 	public Player_Movement movement;
 
 	void OnCollisionEnter(Collision collisionInfo) {
+		Debug.Log("Collision with " + collisionInfo.collider.tag);
 		if(collisionInfo.collider.tag == "Obstacle") {
 			movement.enabled = false;
 			FindObjectOfType<Game_Manager>().EndGame();
 		}
+		if(collisionInfo.collider.tag == "Ground") {
+			movement.isTouchingGround = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+			movement.isTouchingGround = false;
 	}
 }
 	
--- Assets/scripts/Player_Movement.cs
+++ Assets/scripts/Player_Movement.cs
@@ -5,31 +5,40 @@ public class Player_Movement : MonoBehaviour {
     public Rigidbody rigid_body;
     public float forward_force = 2000f;
     public float side_force = 500f;
-
+    public float jump_force = 3000f;
     float ground_level = .99f;
+
+    public bool isTouchingGround = true;
 
 	// Use this for initialization
 	void Start ()
     {
 	}
+
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        rigid_body.AddForce(0, 0, forward_force * Time.deltaTime);
+        if(isTouchingGround) {
 
-        if ( Input.GetKey("d") )
-        {
-            rigid_body.AddForce(side_force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
+            rigid_body.AddForce(0, 0, forward_force * Time.deltaTime);
+
+            if ( Input.GetKey("d") )
+            {
+                rigid_body.AddForce(side_force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                rigid_body.AddForce(-side_force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+
+            if(Input.GetKey("space")) {
+                rigid_body.AddForce(0, jump_force * Time.deltaTime, 0);
+            }
         }
 
-        if (Input.GetKey("a"))
-        {
-            rigid_body.AddForce(-side_force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if(rigid_body.position.y < ground_level)
-        {
+        if(rigid_body.position.y < ground_level) {
             FindObjectOfType<Game_Manager>().EndGame();
         }
     }

bf0ab0f03528800bb07587308265c13bd20490e2
Giancarlo Sanz <gsanz95@gmail.com>
2018-08-18 19:46:19 -0500

    UI
    Added Level Completion UI and function to move onto next level. Animations included

--- Assets/scripts/Game_Manager.cs
+++ Assets/scripts/Game_Manager.cs
@@ -3,13 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour {
 	bool game_end = false;
+	public GameObject complete_UI;
+
+	public void endLevel() {
+		complete_UI.SetActive(true);
+	}
 	float restart_delay = 1f;
 
 	public void EndGame() {
 		if(game_end == false) {
 			game_end = true;
-			//Debug.Log("Game Over!");
-
+			//Debug.Log("Game Over!"); 
 			Invoke("Restart",restart_delay);
 		}
 	}
---
+++ Assets/scripts/LevelEndTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndTrigger : MonoBehaviour {
+	public Game_Manager game_Manager;
+	float restart_delay = 1f;
+
+	void OnTriggerEnter(Collider colliderInfo) {
+		if(colliderInfo.tag == "Player")
+		{
+			//Debug.Log("Game End!");
+			game_Manager.endLevel();
+			Invoke("LoadNextLevel", restart_delay);
+		}
+	}
+
+	void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+}
